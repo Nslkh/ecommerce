ea08ae766dde4b83aa83cad37059a81740ac2b06
Nslkh <olluzran@gmail.com>
2021-03-03 22:53:58 +0500

    Product Table migration and seeding

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('products')->insert([
+            [
+                'name'=>'Iphone 12 Pro-Max',
+                'price'=>'1000',
+                'description'=>('IPhone 12 Pro Max 6.7 Super Retina XDR display2'),
+                'category'=>"mobile",
+                'gallery'=>"https://hosty.xxx/i/4cf22c047214e9219c7a91136113b1d646db6fea.jpg",
+            ],
+            [
+                'name'=>'Iphone 12 Pro',
+                'price'=>'1099',
+                'description'=>('Apple iPhone 12 Pro · Released 2020, October 23 · 189g, 7.4mm thickness · iOS 14.1 '),
+                'category'=>"mobile",
+                'gallery'=>"https://hosty.xxx/i/ed8e7d23eee2900300ccf3a0856b992e1b71df5d.jpg",
+            ],
+            [
+                'name'=>'MacBook Pro',
+                'price'=>'1,699.00',
+                'description'=>('Apple M1 chip with 8‑core CPU, 8-core GPU and 16-core Neural Engine'),
+                'category'=>"Laptop",
+                'gallery'=>"https://hosty.xxx/i/7f4dcda4bbc13cca67360d519608653b5ba6e2ce.jpg",
+            ],
+            [
+                'name'=>'MacBook Air',
+                'price'=>'1,299.00 ',
+                'description'=>('Apple M1 chip with 8‑core CPU, 7‑core GPU and 16‑core Neural Engine'),
+                'category'=>"Laptop",
+                'gallery'=>"https://hosty.xxx/i/7edef94a021014b06b39e70c0d8d4b9aec60547b.jpg",
+            ],
+            [
+                'name'=>'iPad Air 2020 ',
+                'price'=>'749.00',
+                'description'=>('Enjoy by  using our Products '),
+                'category'=>"iPad",
+                'gallery'=>"https://hosty.xxx/i/fdadbc260ed249ddcb0b89bbac3cb72d0a6cb2d5.jpg",
+            ],
+            [
+                'name'=>'iPad Pro ',
+                'price'=>'749.00',
+                'description'=>('You can take a notes , Fantastic '),
+                'category'=>"iPad",
+                'gallery'=>"https://hosty.xxx/i/eb0a0c437618887519c22b7cab1a336e4a668dd6.jpg",
+            ],
+            [
+                'name'=>'Apple iMac ',
+                'price'=>'14.099',
+                'description'=>(' Display Display 21.5-inch (diagonal) LED-backlit display ·'),
+                'category'=>"iMac",
+                'gallery'=>"https://hosty.xxx/i/eb0a0c437618887519c22b7cab1a336e4a668dd6.jpg",
+            ]
+        ]);
+
+    }
+}
