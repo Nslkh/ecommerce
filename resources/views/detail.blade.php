183e6dd56dd5c9ad55fac4432526f940c7702b04
Nslkh <olluzran@gmail.com>
2021-03-04 23:48:12 +0500

    Add product in cart table

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -7,12 +7,16 @@
         </div>
         <div class="col-sm-6">
             <a href="/">Go Back</a>
-            <h2>{{$product['name']}}</h2>
+            <h2 >{{$product['name']}}</h2>
             <h3>Price :{{$product['price']}}</h3>
             <h4>Description :{{$product['description']}}</h4>
             <h4>Category :{{$product['category']}}</h4>
             <br><br>
-            <button class="btn btn-primary">Add to Cart</button>
+            <form action="/add_to_cart" method="POST">
+                @csrf
+                <input type="hidden" name="product_id" value="{{$product['id']}}">
+                <button class="btn btn-primary">Add to Cart</button>
+            </form>
             <br><br>
             <button class="btn btn-success">Buy Now</button>
             <br><br>
